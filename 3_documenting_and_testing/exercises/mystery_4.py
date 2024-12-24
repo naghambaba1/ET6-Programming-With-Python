b8bc974ccbebf3ded44806c03d4f016b6397818e
Nagham al baba <142064107+naghambaba1@users.noreply.github.com>
2024-12-24 23:39:12 +0200

    Update mystery_4.py

--- 3_documenting_and_testing/exercises/mystery_4.py
+++ 3_documenting_and_testing/exercises/mystery_4.py
@@ -7,3 +7,23 @@ def mystery_4(a):
         c = c + 1
 
     return b
+""" exapmle:
+a=3
+the process:
+-3>0 true
+- we add c which is 0 to b
+-b=[0]
+Now c =1 
+-3>1? true 
+-we add c which is 1 to b
+-b=[0,1]
+Now c =2
+-3>2? true 
+-we add c which is 2 to b
+-b=[0,1,2]
+Now c =3
+-3>3? False
+THEN we get out of the while loop AND return b 
+the OUTPUT b = [0,1,2]
+"""
+
