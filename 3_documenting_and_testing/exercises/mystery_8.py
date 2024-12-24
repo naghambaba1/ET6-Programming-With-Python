99b4592e5a730cff9815cde9a0e852a8b44e28fe
Nagham al baba <142064107+naghambaba1@users.noreply.github.com>
2024-12-24 23:59:01 +0200

    Update mystery_8.py

--- 3_documenting_and_testing/exercises/mystery_8.py
+++ 3_documenting_and_testing/exercises/mystery_8.py
@@ -1,7 +1,20 @@
 def mystery_8(a, b):
+     """
+    Returns a list of all characters in 'a' that match 'b'.
+
+    Args:
+        a (str): The input string.
+        b (str): The character to search for.
+
+    Returns:
+        list: A list of all occurrences of 'b' in 'a'.
+    """
     c = []
     while a:
         if b in a[0]:
             c.append(a[0])
         a = a[1:]
     return c
+""" example 
+ mystery_8("nagham", "a")
+ output: ["a","a"]
