817fad5f6f2c9e183c08f6e8378c409657955fae
Nagham al baba <142064107+naghambaba1@users.noreply.github.com>
2024-12-24 23:29:16 +0200

    Update mystery_2.py

--- 3_documenting_and_testing/exercises/mystery_2.py
+++ 3_documenting_and_testing/exercises/mystery_2.py
@@ -1,4 +1,8 @@
 def mystery_2(a):
+    """
+    Returns the length of the input if it is non-empty; otherwise, returns None.
+    Example: a="nagham" then the output will be 6
+    """
     if len(a) == 0:
         return None
 
